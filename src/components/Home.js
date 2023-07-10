b7a20a3c5fc7250f96e11048aea1e16c37574156
saiteja <saitejarailla@gmail.com>
2023-07-10 19:55:44 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,6 @@ function Home() {
                 <div className='home'>
                     <div className='content'>
                         <h2>Stock Alerter</h2>
-                        <ContactUs sent={['saitejarailla@gmail.com',100,'saitejarailla@gmail.com']} />
-                        <ContactUs sent={['saitejarailla@gmail.com',100,'saitejarailla@gmail.com']} />
                         <p>Welcome to Stock Alerter, your go-to platform for staying informed about stock market updates and receiving timely alerts. Whether you're a seasoned investor or just starting out, Stock Alerter provides you with the latest news, real-time data, and personalized notifications to help you make informed investment decisions.</p>
                         <p>Our comprehensive platform offers a wide range of features, including:</p>
                         <ul>
